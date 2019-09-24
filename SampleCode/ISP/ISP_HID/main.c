c944ba1f2e99853e7a1dddca9dbcb02a019cdd99
yjchen5 <yjchen5@nuvoton.com>
2019-09-24 18:51:14 +0800

    Add ISP_DFU and ISP_HID sample code.
    Change-Id: Id09214f453a476ac81b4036bd39aaf11dfc9fb5a

---
+++ SampleCode/ISP/ISP_HID/main.c
@@ -0,0 +1,107 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ *
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "targetdev.h"
+
+#define PLLCON_SETTING          CLK_PLLCON_144MHz_HXT
+#define PLL_CLOCK               144000000
+
+void SYS_Init(void)
+{
+    /* Enable XT1_OUT (PF.0) and XT1_IN (PF.1) */
+    SYS->GPF_MFP &= ~(SYS_GPF_MFP_PF0_Msk | SYS_GPF_MFP_PF1_Msk);
+    SYS->GPF_MFP |= SYS_GPF_MFP_PF0_XT1_OUT | SYS_GPF_MFP_PF1_XT1_IN;
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 22.1184MHz clock */
+    CLK->PWRCON |= CLK_PWRCON_OSC22M_EN_Msk;
+
+    /* Waiting for Internal RC clock ready */
+    while(!(CLK->CLKSTATUS & CLK_CLKSTATUS_OSC22M_STB_Msk));
+
+    /* Enable external XTAL 12 MHz clock */
+    CLK->PWRCON |= CLK_PWRCON_XTL12M_EN_Msk;
+
+    /* Waiting for external XTAL clock ready */
+    while(!(CLK->CLKSTATUS & CLK_CLKSTATUS_XTL12M_STB_Msk));
+
+    /* Select HCLK clock source as HIRC and HCLK clock divider as 1 */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLK_S_Msk)) | CLK_CLKSEL0_HCLK_S_HIRC;
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_HCLK_N_Msk)) | CLK_CLKDIV_HCLK(1);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK->PLLCON = PLLCON_SETTING;
+
+    while(!(CLK->CLKSTATUS & CLK_CLKSTATUS_PLL_STB_Msk));
+
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_HCLK_N_Msk)) | CLK_CLKDIV_HCLK(2);
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLK_S_Msk)) | CLK_CLKSEL0_HCLK_S_PLL;
+
+    /* Update System Core Clock */
+    PllClock        = PLL_CLOCK;                // PLL
+    SystemCoreClock = PLL_CLOCK / 2;            // HCLK
+    CyclesPerUs     = PLL_CLOCK / 2 / 1000000;  // For CLK_SysTickDelay()
+
+    /* Enable module clock */
+    CLK->APBCLK |= CLK_APBCLK_USBD_EN_Msk;
+    CLK->CLKDIV = (CLK->CLKDIV & (~CLK_CLKDIV_USB_N_Msk)) | CLK_CLKDIV_USB(3);
+}
+
+void USBD_IRQHandler(void);
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    /* Unlock write-protected registers */
+    SYS_UnlockReg();
+
+    /* Init system and multi-function I/O */
+    SYS_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISP_EN_Msk;
+    FMC->ISPCON |= FMC_ISPCON_ISPEN_Msk;
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+
+    while(DetectPin == 0)
+    {
+        /* Open USB controller */
+        USBD_Open(&gsInfo, HID_ClassRequest, NULL);
+        /*Init Endpoint configuration for HID */
+        HID_Init();
+        /* Start USB device */
+        USBD_Start();
+
+        /* DO NOT Enable USB device interrupt */
+        // NVIC_EnableIRQ(USBD_IRQn);
+
+        while(DetectPin == 0)
+        {
+            // polling USBD interrupt flag
+            USBD_IRQHandler();
+
+            if(bUsbDataReady == TRUE)
+            {
+                ParseCmd((uint8_t *)usb_rcvbuf, 64);
+                EP2_Handler();
+                bUsbDataReady = FALSE;
+            }
+        }
+    }
+
+    SYS->RSTSRC = (SYS_RSTSRC_RSTS_POR_Msk | SYS_RSTSRC_RSTS_RESET_Msk);//clear bit
+    FMC->ISPCON &=  ~(FMC_ISPCON_ISPEN_Msk | FMC_ISPCON_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while(1);
+}
+
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
