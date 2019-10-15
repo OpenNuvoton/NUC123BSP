fc2f47552d40b6d1d4278cd4a3012035291ecc3d
SFWU <SFWU@nuvoton.com>
2019-10-15 16:14:52 +0800

    Update GetApromSize() and GetDataFlashInfo().
    Change-Id: I2d03267471a464978fe5b274ee5385032f4e85e6

--- SampleCode/ISP/ISP_I2C/targetdev.c
+++ SampleCode/ISP/ISP_I2C/targetdev.c
@@ -2,7 +2,6 @@
  * @file     targetdev.c
  * @brief    ISP support function source file
  * @version  0x32
- * @date     14, June, 2017
  *
  * @note
  * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
@@ -10,55 +9,50 @@
 #include "targetdev.h"
 #include "isp_user.h"
 
+#define CONFIG0_DFEN                0x01
+#define CONFIG0_DFVSEN              0x04
+
+
 uint32_t GetApromSize()
 {
-    //the smallest of APROM size is 2K
-    uint32_t size = 0x800, data;
+    uint32_t size = 0xA000, data;
     int result;
+    result = FMC_Read_User(size, &data);
 
-    do
-    {
-        result = FMC_Read_User(size, &data);
-
-        if(result < 0)
-        {
-            return size;
-        }
-        else
-        {
-            size *= 2;
-        }
+    if (result < 0) {
+        return 32 * 1024;
+    } else {
+        return 64 * 1024;
     }
-    while(1);
 }
 
-// Data Flash is shared with APROM.
-// The size and start address are defined in CONFIG1.
 void GetDataFlashInfo(uint32_t *addr, uint32_t *size)
 {
     uint32_t uData;
+    g_apromSize = GetApromSize();
     *size = 0;
+    /* Note: DFVSEN = 1, DATA Flash Size is 4K bytes
+             DFVSEN = 0, DATA Flash Size is based on CONFIG1 */
     FMC_Read_User(Config0, &uData);
 
-    if((uData & 0x01) == 0)    //DFEN enable
-    {
+    if (uData & CONFIG0_DFVSEN) {
+        *addr = 0x1F000;
+        *size = 4096;//4K
+    } else if (uData & CONFIG0_DFEN) {
+        g_apromSize += 4096;
+        *addr = g_apromSize;
+        *size = 0;
+    } else {
+        g_apromSize += 4096;
         FMC_Read_User(Config1, &uData);
 
-        // Filter the reserved bits in CONFIG1
-        uData &= 0x000FFFFF;
-
-        if(uData > g_apromSize || uData & (FMC_FLASH_PAGE_SIZE - 1))    //avoid config1 value from error
-        {
+        if (uData > g_apromSize || (uData & 0x1FF)) { //avoid config1 value from error
             uData = g_apromSize;
         }
 
         *addr = uData;
         *size = g_apromSize - uData;
-    }
-    else
-    {
-        *addr = g_apromSize;
-        *size = 0;
+        g_apromSize -= *size;
     }
 }
 
